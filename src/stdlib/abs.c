fa624bf414aa9861b77bd64d915e222be732b80e
Darragh Downey <darragh.downey90@gmail.com>
2024-11-05 20:09:32 +1100

    updated abs.c to securely calculate the absolute value of an int on two's complement systems. TODO: update to handle additional cases

--- src/stdlib/abs.c
+++ src/stdlib/abs.c
@@ -1,6 +1,15 @@
 #include <stdlib.h>
+#include <limits.h>
 
+// Safely returns the absolute value of a given integer.
 int abs(int a)
 {
-	return a>0 ? a : -a;
+	// flipping INT_MIN results in INT_MIN, so we need to handle it separately
+	if (a == INT_MIN)
+		return INT_MAX;
+
+	// if a is negative, mask will be 0xFFFFFFFF, otherwise it will be 0x00000000
+	int mask = a >> (sizeof(int) * CHAR_BIT - 1);
+	// XOR with mask will flip all bits if a is negative, otherwise it will do nothing
+	return (a + (-mask)) ^ mask;
 }
